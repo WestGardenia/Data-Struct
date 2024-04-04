3933a754726f6566270c21aba1a51cd0c9e5a4b1
停在花开之时 <165155834+WestGardenia@users.noreply.github.com>
2024-04-04 17:53:45 +0800

    2024.4.4

--- Stack/Stack/Function.h
+++ Stack/Stack/Function.h
@@ -9,14 +9,15 @@ typedef struct Stack SK;
 
 struct Stack
 {
-	SKDataType* a;
-	int top;	// 栈顶位置
+	SKDataType* a;	// 数组栈对应的数组名->a
+	int top_val;	// 栈顶位置
 	int capacity;	// 栈的容量
 };
 
 void StackInit(SK* ps);	// 初始化栈
 
 void StackDestroy(SK* ps);	// 销毁栈
+
 
 void StackPush(SK* ps,SKDataType x);	// 压栈操作
 
@@ -27,4 +28,11 @@ SKDataType StackTop(SK* ps);	// 获得栈顶元素
 int StackSize(SK* ps);	// 获得栈的元素个数
 
 bool StackEmpty(SK* ps);	// 判断栈是否为空栈
+
+bool isValid(char* s);	// 判断字符串内是否为有效括号，并对其进行匹配
+// (  匹配  )
+// [  匹配  ]
+// {  匹配  }
+// 满足左右括号相匹配的同时，还需要满足对应括号的类别相匹配
+//
 
--- Stack/Stack/test.c
+++ Stack/Stack/test.c
@@ -3,11 +3,55 @@
 
 void test01()
 {
+	SK st;
+	StackInit(&st);
+	StackPush(&st, 1);
+	StackPush(&st, 2);
+	StackPush(&st, 3);
+	StackPush(&st, 4);
+	StackPush(&st, 5);
+	StackPush(&st, 6);
 
+	StackPop(&st);
+	StackPop(&st);
+
+	printf("%d\n", StackTop(&st));
+	
+	StackDestroy(&st);	// 每次使用完栈需要将其销毁。否则会造成空间占用
+
+}
+
+
+void test02()
+{
+	SK st;
+	StackInit(&st);
+	StackPush(&st, 1);
+	StackPush(&st, 2);
+	StackPush(&st, 3);
+	StackPush(&st, 4);
+	StackPush(&st, 5);
+
+	printf("%d ", StackTop(&st));
+	StackPop(&st);
+	
+	printf("%d ", StackTop(&st));
+	StackPop(&st);
+
+	StackPush(&st, 6);
+
+	while (!StackEmpty(&st))
+	{
+		printf("%d ", StackTop(&st));	// 取栈顶数据，并将其打印
+		StackPop(&st);	// 每次取出栈顶数据后，将栈顶位置迭代，得到新的栈顶
+	}
+
+	StackDestroy(&st);
 }
 
 int main()
 {
 	test01();
+	test02();
 	return 0;
 }
