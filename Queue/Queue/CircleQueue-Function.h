9020938b0dbd09f07d0d1388a2cb8be92212207b
停在花开之时 <165155834+WestGardenia@users.noreply.github.com>
2024-04-09 18:16:33 +0800

    2024.4.9

--- Queue/Queue/CircleQueue-Function.h
+++ Queue/Queue/CircleQueue-Function.h
@@ -4,28 +4,59 @@
 #include<assert.h>
 #include<stdbool.h>
 
+// 设计你的循环队列实现。 循环队列是一种线性数据结构，其操作表现基于 FIFO（先进先出）原则并且队尾被连接在队首之后以形成一个循环。它也被称为“环形缓冲器”。
 
+// 循环队列的一个好处是我们可以利用这个队列之前用过的空间。
+// 在一个普通队列里，一旦一个队列满了，我们就不能插入下一个元素，即使在队列前面仍有空间。
+// 但是使用循环队列，我们能使用这些空间去存储新的值。这些空间往往是曾今存储过元素但是又经过删除后变为了空的空间
+
+// 1、符合先进先出的特点
+// 2、空间大小是固定的
+// 3、可以用链表的指针控制表示循环结构；也可以使用数组的下标控制表示循环结构
+
+
+// 重点：
+// 循环队列无论是使用数组还是链表实现，都要多开一个空间
+// 也就意味着，要存k个数据的循环队列，要开k+1个空间
+// 否则无法实现判空和判满
+// 
+// 例如：
+// 当使用链式结构实现循环队列。
+// 判空的条件自然是tail == head。
+// 但是当没有多余空间去判断时，tail == head也可以是判满的条件。
+//	当创建了新空间时，判满条件为：
+//		数组实现：(tail+1)%(k+1) == head
+//		链表实现： tail->next == head
+// 
+// 
+// ————————————————————————————————————————————————————————————————————————
+// 
+// 数组结构实现环形队列
+//
 typedef struct MyCircularQueue MyCircularQueue;
 
 struct MyCircularQueue {
-
+	int* a;		// 数组结构
+	int front;
+	int tail;
+	int k;	// 环形队列的大小
 };
 
-MyCircularQueue* myCircularQueueCreate(int k);
+MyCircularQueue* myCircularQueueCreate(int k);	// 创建循环队列
 
-bool myCircularQueueEnQueue(MyCircularQueue* obj, int value);
+bool myCircularQueueEnQueue(MyCircularQueue* obj, int value);	// 判断是否成功插入元素
 
-bool myCircularQueueDeQueue(MyCircularQueue* obj);
+bool myCircularQueueDeQueue(MyCircularQueue* obj);	// 判断是否成功删除元素
 
-int myCircularQueueFront(MyCircularQueue* obj);
+int myCircularQueueFront(MyCircularQueue* obj);	// 循环队列--取对头数据
 
-int myCircularQueueRear(MyCircularQueue* obj);
+int myCircularQueueRear(MyCircularQueue* obj);	// 循环队列--取队尾数据
 
-bool myCircularQueueIsEmpty(MyCircularQueue* obj);
+bool myCircularQueueIsEmpty(MyCircularQueue* obj);	// 循环队列--判空
 
-bool myCircularQueueIsFull(MyCircularQueue* obj);
+bool myCircularQueueIsFull(MyCircularQueue* obj);	// 循环队列--判满
 
-void myCircularQueueFree(MyCircularQueue* obj);
+void myCircularQueueFree(MyCircularQueue* obj);	// 销毁循环队列
 
 /*
  * Your MyCircularQueue struct will be instantiated and called as such:
--- Queue/Queue/CircleQueue.c
+++ Queue/Queue/CircleQueue.c
@@ -1,5 +1,77 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include"CircleQueue-Function.h"
 
+MyCircularQueue* myCircularQueueCreate(int k)	// 创建循环队列
+{
+	MyCircularQueue* cq = (MyCircularQueue*)malloc(sizeof(MyCircularQueue));
+	cq->a = malloc(sizeof(int) * (k + 1));	// 多开一个空间区分空和满
+	cq->front = cq->tail = 0;
+	cq->k = k;
 
+	return cq;
+}
 
+bool myCircularQueueEnQueue(MyCircularQueue* obj, int value)	// 判断是否成功插入元素
+{
+	if (myCircularQueueIsFull(obj))
+		// 如果循环队列已满，则无可用空间去插入数据，则判断插入失败。返回false
+		return false;
+	obj->a[obj->tail] = value;
+	obj->tail++;
+	obj->tail %= (obj->k + 1);	// 控制下标，防止下标越界
+	
+	return true;
+}
+
+bool myCircularQueueDeQueue(MyCircularQueue* obj)	// 判断是否成功删除元素
+{
+	if (myCircularQueueIsEmpty(obj))
+		// 如果循环队列已空，则无可用数据去删除，则判断删除失败。返回false
+		return false;
+	// 删除的情况相对简单
+	// 由于循环队列的特点，只需要将front下标自加，那么原来front对应的数据就会被未来插入时的数据覆盖。同时不会被意外访问
+	obj->front++;
+	obj->front %= (obj->k + 1);
+
+	return true;
+}
+
+int myCircularQueueFront(MyCircularQueue* obj)	// 循环队列--取对头数据
+{
+	if (myCircularQueueIsEmpty(obj))
+		return -1;
+	return obj -> a[obj->front];
+}
+
+int myCircularQueueRear(MyCircularQueue* obj)	// 循环队列--取队尾数据
+{
+	if (myCircularQueueIsEmpty(obj))
+		return -1;
+	// 取队尾数据时，由于tail的前一个数据才是所求的队尾数据
+	// 而循环队列的tail和队尾数据的相对位置并不统一。所以需要进行处理一下
+	// 1、数据处理
+	int realtail = (obj->tail+obj->k)%(obj->k+1);
+	return obj->a[realtail];
+	// 2、暴力判断
+	//if (obj->tail == 0)
+	//	return obj->a[obj->k];
+	//else
+	//	return obj->a[obj->tail - 1];
+
+}
+
+bool myCircularQueueIsEmpty(MyCircularQueue* obj)	// 循环队列--判空
+{
+	return obj->front == obj->tail;
+}
+
+bool myCircularQueueIsFull(MyCircularQueue* obj)	// 循环队列--判满
+{
+	return (obj->tail + 1) % (obj->k + 1) == obj->front;
+}
+
+void myCircularQueueFree(MyCircularQueue* obj)	// 销毁循环队列
+{
+	free(obj->a);
+	free(obj);
+}
